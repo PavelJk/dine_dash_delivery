2470bfcc52c132509f98c006875774395bcd6ef9
PavelJk <pavel.2021@list.ru>
2025-02-11 22:16:48 +0300

    Add empty screen

---
+++ lib/src/feature/auth/screens/registr_number.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegistrNumberScreen extends StatelessWidget {
+  const RegistrNumberScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
