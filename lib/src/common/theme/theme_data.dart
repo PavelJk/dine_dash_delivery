9967644e35a2273c2b50d492d99f48ac69af8f8b
PavelJk <pavel.2021@list.ru>
2025-02-12 23:05:29 +0300

    Add style, color,  font

--- lib/src/common/theme/theme_data.dart
+++ lib/src/common/theme/theme_data.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final ThemeData lightTheme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.light(
-    primary: Color(0xffFFE500),
+    primary: Color(0xff32343E),
     secondary: Color(0xff121223),
-    tertiary: Color(0xff32343E),
+    tertiary: Color(0xffFFE500),
     surface: Color(0xffF0F5FA),
+    inversePrimary: Color(0xffffffff)
   ),
-  textTheme: textTheme,
+  textTheme: GoogleFonts.senTextTheme(textTheme),
   scaffoldBackgroundColor: Color(0xffffffff),
   
 );
@@ -21,7 +23,7 @@ final ThemeData darkTheme = ThemeData(
     tertiary: Color(0xff32343E),
     surface: Color(0xffF0F5FA),
   ),
-  textTheme: textTheme,
+  textTheme: GoogleFonts.senTextTheme(textTheme),
   scaffoldBackgroundColor: Colors.black,
 );
 
@@ -29,16 +31,19 @@ final TextTheme textTheme = TextTheme(
     headlineLarge: TextStyle(
       fontSize: 30,
       fontWeight: FontWeight.bold,
+      color: Colors.white
     ),
     headlineMedium: TextStyle(
       fontSize: 16,
       fontWeight: FontWeight.w400,
+      color: Colors.white.withValues(alpha: 85)
     ),
     headlineSmall: TextStyle(
 
     ),
     bodyLarge: TextStyle(
       fontSize: 14,
+      color: Color(0xff32343E),
       fontWeight: FontWeight.w400,
     ),
     bodyMedium: TextStyle(
@@ -50,5 +55,12 @@ final TextTheme textTheme = TextTheme(
     titleMedium: TextStyle(
       fontSize: 13,
       fontWeight: FontWeight.w400,
+      letterSpacing: 0.3,
+      color: Color(0xff32343E),
+    ),
+    labelMedium: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+      color: Color(0xff121223)
     )
 );
