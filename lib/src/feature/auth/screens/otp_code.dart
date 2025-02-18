ddf7dca6e7c71670c08a5afb0757e0249300897e
PavelJk <pavel.2021@list.ru>
2025-02-18 22:44:19 +0300

    Add page otp code

--- lib/src/feature/auth/screens/otp_code.dart
+++ lib/src/feature/auth/screens/otp_code.dart
@@ -1,27 +1,86 @@
+import 'dart:async';
 import 'package:dine_dash_delivery/src/common/resources/path_images.dart';
+import 'package:dine_dash_delivery/src/common/router/router.dart';
+import 'package:dine_dash_delivery/src/feature/auth/validators/validators.dart';
+import 'package:dine_dash_delivery/src/feature/widgets/appbar_widget.dart';
 import 'package:dine_dash_delivery/src/feature/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pinput/pinput.dart';
 
-class OTPCode extends StatelessWidget {
+class OTPCode extends StatefulWidget {
   final String phoneNumber;
 
   const OTPCode({super.key, required this.phoneNumber});
 
   @override
+  State<OTPCode> createState() => _OTPCodeState();
+}
+
+class _OTPCodeState extends State<OTPCode> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  int _currentTimerValue = 60;
+  bool _isResendCode = false;
+  late Timer _timer;
+
+  void resend(){
+    setState(() {
+      _isResendCode = true;
+    });
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        setState(() {
+          if (_currentTimerValue == 0) {
+            _currentTimerValue = 60;
+            _isResendCode = false;
+            timer.cancel();
+          }else{
+            _currentTimerValue--;
+          }
+        });
+     }
+     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    resend();
+  }
+
+  @override
+  void dispose() {
+    if(_timer.isActive){
+      _timer.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       backgroundColor: Theme.of(context).colorScheme.secondary,
+      appBar: AppbarWidget(
+        onTap: () {
+          context.goNamed(AppRoute.registrPhone.name);
+        },
+      ),
       body: Stack(
         children: [
           Positioned(
-            child: Image.asset(
+            child: SvgPicture.asset(
               PathImages.ellipseAuth,
+              width: 155,
             ),
           ),
           Positioned(
-            right: 2,
-            child: Image.asset(
+            right: 0,
+            child: SvgPicture.asset(
               PathImages.vectorAuth,
+              width: 98,
             ),
           ),
           Padding(
@@ -29,13 +88,13 @@ class OTPCode extends StatelessWidget {
             child: Column(
               children: [
                 Text(
-                  "Введи код",
+                  "Введите код",
                   style: Theme.of(context).textTheme.headlineLarge,
                 ),
-                SizedBox(height: 10,),
+                SizedBox(height: 6,),
                 Text(
                   textAlign: TextAlign.center,
-                  "Мы оправили код на ваш номер\n$phoneNumber",
+                  "Мы оправили код на ваш номер\n${widget.phoneNumber}",
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
                 SizedBox(height: 27,),
@@ -53,15 +112,68 @@ class OTPCode extends StatelessWidget {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              "КОД",
-                              style: Theme.of(context).textTheme.titleMedium,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "КОД",
+                                  style: Theme.of(context).textTheme.titleMedium,
+                                ),
+                                _isResendCode ?
+                                Text(
+                                  "Получить повторно: $_currentTimerValue сек.",
+                                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                                    fontSize: 14,
+                                  ),
+                                ):
+                                SizedBox(
+                                  height: 21,
+                                  child: TextButton(
+                                    onPressed: (){
+                                      setState(() {
+                                        if (_isResendCode) return;
+                                        resend();
+                                      });
+                                    },
+                                    style: TextButton.styleFrom(
+                                      splashFactory: NoSplash.splashFactory,
+                                      padding: EdgeInsets.zero,
+                                    ),
+                                    child: Text(
+                                      'Отправить повторно',
+                                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                                      fontSize: 14,
+                                      decoration: TextDecoration.underline,
+                                    ),
+                                    )
+                                  ),
+                                ),
+                              ],
                             ),
-                            SizedBox(height: 6,),
+                            SizedBox(height: 8,),
+                            Form(
+                              key: _formKey,
+                              child: Pinput(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                                defaultPinTheme: PinTheme(
+                                  width: 70,
+                                  height: 70,
+                                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Theme.of(context).colorScheme.surface,
+                                  )
+                                ),
+                                validator: (pin) => Validator.oTPCode(pin),
+                              ),
+                            ),
                             Spacer(),
                             CustomButtonWidget(
                               onPressed: (){
-
+                                if (_formKey.currentState!.validate()) {
+                                  context.goNamed(AppRoute.registrInfoAboutMe.name);
+                                }
                               },
                             )
                           ],
