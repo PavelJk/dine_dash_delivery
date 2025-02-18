74a51227855fb85f0c32d9b1a4409114ff5dff8e
PavelJk <pavel.2021@list.ru>
2025-02-18 22:42:04 +0300

    Add new svg icon

--- lib/src/common/resources/path_images.dart
+++ lib/src/common/resources/path_images.dart
@@ -1,4 +1,6 @@
 class PathImages {
-  static const String ellipseAuth = "assets/images/ellipse.png";
-  static const String vectorAuth = "assets/images/vector.png";
+  static const String ellipseAuth = "assets/images/ellipse.svg";
+  static const String vectorAuth = "assets/images/vector.svg";
+  static const String back = "assets/icons/back.svg";
+  static const String happybirsday = "assets/icons/happybisday.svg";
 }
