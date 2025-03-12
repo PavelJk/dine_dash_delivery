728ca2c5a068043a0694a343d4c6de0440d83def
PavelJk <pavel.2021@list.ru>
2025-03-12 11:08:56 +0300

    Change path appbar widget

--- lib/src/feature/auth/widgets/appbar_widget.dart
+++ lib/src/feature/auth/widgets/appbar_widget.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-import '../../common/resources/path_images.dart';
+import '../../../common/resources/path_images.dart';
 
 class AppbarWidget extends StatelessWidget implements  PreferredSizeWidget {
 
   final void Function()? onTap;
-   const AppbarWidget({super.key, this.onTap});
+  final Color? backgroundColor;
+  final Color iconColor;
+  const AppbarWidget({super.key, this.onTap, this.backgroundColor, required this.iconColor});
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +18,13 @@ class AppbarWidget extends StatelessWidget implements  PreferredSizeWidget {
         child: GestureDetector(
           onTap: onTap,
           child: CircleAvatar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            backgroundColor: backgroundColor,
             child: Padding(
               padding: const EdgeInsets.only(right: 1),
               child: SvgPicture.asset(
                 PathImages.back,
                 width: 8,
+                colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
               ),
             ),
           ),
