f7a59a581c77491897674aee107e6d718355b41f
PavelJk <pavel.2021@list.ru>
2025-03-12 11:11:44 +0300

    Add new icon and image

--- lib/src/common/resources/path_images.dart
+++ lib/src/common/resources/path_images.dart
@@ -3,4 +3,13 @@ class PathImages {
   static const String vectorAuth = "assets/images/vector.svg";
   static const String back = "assets/icons/back.svg";
   static const String happybirsday = "assets/icons/happybisday.svg";
+  static const String geoLocation = "assets/images/geo_location.png";
+  static const String mapPin = "assets/icons/map_pin.svg";
+  static const String menu = "assets/icons/menu.svg";
+  static const String corzina = "assets/icons/corzina.svg";
+  static const String search = "assets/icons/search.svg";
+  static const String video = "assets/icons/video.svg";
+  static const String product = "assets/images/product.png";
+  static const String restoran = "assets/images/restoran.png";
+  static const String forward = "assets/icons/forward.svg";
 }
