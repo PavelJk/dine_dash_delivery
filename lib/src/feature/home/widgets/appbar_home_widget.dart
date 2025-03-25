1005394df00ff2e9a5ce3e1c5eb96a74208b7dc1
PavelJk <pavel.2021@list.ru>
2025-03-25 14:27:58 +0300

    Change app bar main home

--- lib/src/feature/home/widgets/appbar_home_widget.dart
+++ lib/src/feature/home/widgets/appbar_home_widget.dart
@@ -13,70 +13,76 @@ class MySliverAppbarHomeWidget extends StatelessWidget {
       automaticallyImplyLeading: false,
       scrolledUnderElevation: 0,
       pinned: true,
-      toolbarHeight: 61,
-      title: Padding(
-        padding: const EdgeInsets.only(left: 5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      toolbarHeight: 17,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              'ДОСТАВИТЬ В',
-              style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                fontSize: 12,
+            GestureDetector(
+              onTap: onTap,
+              child: CircleAvatar(
+                radius: 26,
+                backgroundColor: Theme.of(context).colorScheme.onTertiary,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 4),
+                  child: SvgPicture.asset(
+                    PathImages.menu,
+                    width: 22,
+                  ),
+                ),
               ),
             ),
-            dropdownButton,
+            Padding(
+              padding: const EdgeInsets.only(right: 7, top: 3),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'ДОСТАВИТЬ В',
+                    style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                      fontSize: 12,
+                    ),
+                  ),
+                  dropdownButton,
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+              },
+              child: Stack(
+                children: [
+                  SizedBox(
+                    width: 50,
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      child: SvgPicture.asset(
+                        PathImages.corzina,
+                        width: 24,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Theme.of(context).colorScheme.tertiary,
+                      child: Text(
+                        '2',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontSize: 20
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
-      leading: GestureDetector(
-        onTap: onTap,
-        child: CircleAvatar(
-          radius: 28,
-          backgroundColor: Theme.of(context).colorScheme.onTertiary,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 3),
-            child: SvgPicture.asset(
-              PathImages.menu,
-              width: 21,
-            ),
-          ),
-        ),
-      ),
-      actions: [
-        GestureDetector(
-          onTap: () {
-          },
-          child: Stack(
-            children: [
-              SizedBox(
-                width: 55,
-                child: CircleAvatar(
-                  radius: 31,
-                  backgroundColor: Theme.of(context).colorScheme.secondary,
-                  child: SvgPicture.asset(
-                    PathImages.corzina,
-                    width: 24,
-                  ),
-                ),
-              ),
-               Positioned(
-                right: 0,
-                child: CircleAvatar(
-                  radius: 15,
-                  backgroundColor: Theme.of(context).colorScheme.tertiary,
-                  child: Text(
-                    '2',
-                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      fontSize: 20
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
     );
   }
 }
