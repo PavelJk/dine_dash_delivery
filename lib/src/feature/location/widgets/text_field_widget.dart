ddbc102594266dc987a1627efb03572ee24a7f8d
PavelJk <pavel.2021@list.ru>
2025-03-25 14:26:57 +0300

    Change text field widget

--- lib/src/feature/location/widgets/text_field_widget.dart
+++ lib/src/feature/location/widgets/text_field_widget.dart
@@ -3,27 +3,38 @@ import 'package:flutter/material.dart';
 class MyTextFieldLocation extends StatelessWidget {
   final Widget? prefixIcon;
   final TextEditingController? controller;
+  final String text;
 
-  const MyTextFieldLocation({super.key, this.prefixIcon, this.controller});
+  const MyTextFieldLocation({super.key, this.prefixIcon, this.controller, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        prefixIcon: prefixIcon,
-        contentPadding: EdgeInsets.all(18),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          borderSide: BorderSide.none
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text,
+          style: Theme.of(context).textTheme.bodyLarge,
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          borderSide: BorderSide.none
+        SizedBox(height: 7,),
+        TextFormField(
+          decoration: InputDecoration(
+            prefixIcon: prefixIcon,
+            contentPadding: EdgeInsets.all(18),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderSide: BorderSide.none
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderSide: BorderSide.none
+            ),
+            filled: true,
+            fillColor: Theme.of(context).colorScheme.surface,
+          ),
+          controller: controller,
         ),
-        filled: true,
-        fillColor: Theme.of(context).colorScheme.surface,
-      ),
-      controller: controller,
+      ],
     );
   }
 }
