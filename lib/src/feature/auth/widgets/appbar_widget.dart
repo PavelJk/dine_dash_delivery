0817e4dab50277a5f89e59c9baac662c394d4c62
PavelJk <pavel.2021@list.ru>
2025-03-28 22:25:13 +0300

    Refactor

--- lib/src/feature/auth/widgets/appbar_widget.dart
+++ lib/src/feature/auth/widgets/appbar_widget.dart
@@ -12,6 +12,7 @@ class AppbarWidget extends StatelessWidget implements  PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      scrolledUnderElevation: 0,
       backgroundColor: Colors.transparent,
       leading: Padding(
         padding: const EdgeInsets.only(left: 24),
