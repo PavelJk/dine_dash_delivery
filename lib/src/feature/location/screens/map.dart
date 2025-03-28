66ca70c46eb5a85fbb46b65e05fb23f2a62fb52c
PavelJk <pavel.2021@list.ru>
2025-03-28 22:28:09 +0300

    Сontinuing work page

--- lib/src/feature/location/screens/map.dart
+++ lib/src/feature/location/screens/map.dart
@@ -1,14 +1,17 @@
-import 'dart:async';
-
 import 'package:dine_dash_delivery/src/common/resources/path_images.dart';
 import 'package:dine_dash_delivery/src/common/router/router.dart';
 import 'package:dine_dash_delivery/src/feature/auth/widgets/appbar_widget.dart';
+import 'package:dine_dash_delivery/src/feature/location/widgets/button_widget.dart';
 import 'package:dine_dash_delivery/src/feature/location/widgets/text_field_widget.dart';
+import 'package:dine_dash_delivery/src/feature/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+enum BuildingType { home, work, other }
+
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -20,6 +23,8 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   //final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
   bool _isPanelOpen = false;
+  BuildingType? _selectedBuilding;
+
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(56.631600, 47.886178),
@@ -63,11 +68,12 @@ class _MapScreenState extends State<MapScreen> {
             ),
           ) : SizedBox.shrink(),
           SlidingUpPanel(
-            padding: EdgeInsets.only(top: 6.5, right: 24, left: 24, bottom: 18),
+            padding: EdgeInsets.only(top: 6.5, right: 24, left: 24, bottom: 16),
             backdropEnabled: true,
             backdropOpacity: 0.7,
             backdropColor: Theme.of(context).colorScheme.inversePrimary,
             minHeight: 140,
+            maxHeight: MediaQuery.of(context).viewInsets.bottom > 0 ? 310 : 500,
             borderRadius: BorderRadius.vertical(
               top: Radius.circular(24),
             ),
@@ -76,61 +82,153 @@ class _MapScreenState extends State<MapScreen> {
                 _isPanelOpen = position > 0.03;
               });
             },
-            panel: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    width: 70,
-                    height: 7,
-                    decoration: BoxDecoration(
-                      color: Color(0xffD8E3ED),
-                      borderRadius: BorderRadius.circular(80)
-                    ),
+            onPanelOpened: () => setState(() => _isPanelOpen = true),
+            onPanelClosed: () => setState(() => _isPanelOpen = false),
+            panel: Builder(
+              builder: (context) {
+                final content = Padding(
+                  padding: const EdgeInsets.only(bottom: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      MyLocationTextField(
+                        text: 'АДРЕС ДОСТАВКИ',
+                        prefixIcon: SvgPicture.asset(
+                          PathImages.location,
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                      SizedBox(height: 13,),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: MyLocationTextField(
+                              text: 'КВ/ОФИС',
+                            ),
+                          ),
+                          SizedBox(width: 12,),
+                          Expanded(
+                            child: MyLocationTextField(
+                              text: 'ПОДЪЕЗД'
+                            )
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 13,),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: MyLocationTextField(
+                              text: 'ЭТАЖ',
+                            ),
+                          ),
+                          
+                          SizedBox(width: 12,),
+                          Expanded(
+                            child: MyLocationTextField(
+                              text: 'ДОМОФОН'
+                            )
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 13,),
+                      Text(
+                        'ТИП ЗДАНИЯ',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      SizedBox(height: 7,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          MyTypeBuildingButton(
+                            onTap: () {
+                              setState(() {
+                                _selectedBuilding = _selectedBuilding == BuildingType.home 
+                                    ? null 
+                                    : BuildingType.home;
+                              });
+                            },
+                            text: 'Дом', 
+                            isCheckPressButton: _selectedBuilding == BuildingType.home,
+                          ),
+                          MyTypeBuildingButton(
+                            onTap: () {
+                              setState(() {
+                                _selectedBuilding = _selectedBuilding == BuildingType.work 
+                                    ? null 
+                                    : BuildingType.work;
+                              });
+                            },
+                            text: 'Работа', 
+                            isCheckPressButton: _selectedBuilding == BuildingType.work,
+                          ),
+                          MyTypeBuildingButton(
+                            onTap: () {
+                              setState(() {
+                                _selectedBuilding = _selectedBuilding == BuildingType.other 
+                                    ? null 
+                                    : BuildingType.other;
+                              });
+                            },
+                            text: 'Другое', 
+                            isCheckPressButton: _selectedBuilding == BuildingType.other,
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 28,),
+                      MyCustomButtonWidget(
+                        onPressed: (){
+                  
+                        }, 
+                        text: 'СОХРАНИТЬ АДРЕС'
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(height: 20,),
-                MyTextFieldLocation(
-                  text: 'АДРЕС ДОСТАВКИ',
-                ),
-                SizedBox(height: 13,),
-                Row(
-                  children: [
-                    Expanded(
-                      child: MyTextFieldLocation(
-                        text: 'КВ/ОФИС',
-                      ),
+                );
+                return _isPanelOpen ? CustomScrollView(
+                  slivers: [
+                    SliverAppBar(
+                      pinned: true,
+                      primary: false,
+                      scrolledUnderElevation: 0,
+                      toolbarHeight: 27,
+                      flexibleSpace: MaAppBarSlidingUpPanel(),
                     ),
-                    SizedBox(width: 12,),
-                    Expanded(
-                      child: MyTextFieldLocation(
-                        text: 'ПОДЪЕЗД'
-                      )
+                    SliverToBoxAdapter(
+                      child: content,
                     ),
                   ],
-                ),
-                SizedBox(height: 13,),
-                Row(
+                ) : Column(
                   children: [
-                    Expanded(
-                      child: MyTextFieldLocation(
-                        text: 'ЭТАЖ',
-                      ),
-                    ),
-                    SizedBox(width: 12,),
-                    Expanded(
-                      child: MyTextFieldLocation(
-                        text: 'ДОМОФОН'
-                      )
-                    ),
+                    MaAppBarSlidingUpPanel(),
+                    SizedBox(height: 20,),
+                    content,
                   ],
-                ),
-              ],
-            ),
+                );
+              }
+            )
           ),
         ]
       )
+    );
+  }
+}
+
+class MaAppBarSlidingUpPanel extends StatelessWidget {
+  const MaAppBarSlidingUpPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: 70,
+          height: 7,
+          decoration: BoxDecoration(
+          color: Color(0xffD8E3ED),
+          borderRadius: BorderRadius.circular(80)
+        ),
+      ),
     );
   }
 }
